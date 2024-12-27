484b142a3ff931f9e89a063ab5042e90befacb16
LGCDMBandara <dchathura509@gmail.com>
2024-12-27 20:46:05 +0530

    add comment

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,5 +7,6 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/changePassword', changePassword);
 router.post('/send-otp', sendOtp);
+//sd;fkjls;ldkf;lkj
 
 module.exports = router;
